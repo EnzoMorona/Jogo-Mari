fec1b44c4efd04a35e5c38816bd2363e29ceff34
Enzo Coelho Morona <106177078+EnzoMorona@users.noreply.github.com>
2023-06-21 15:37:53 -0300

    jogo 2
    commit 2

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class Mob : MonoBehaviour
+{
+    public float distanciaMinima = 2;
+    public Transform alvo;
+
+    private NavMeshAgent navMeshAgent;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.SetDestination(alvo.position);
+    }
+}
